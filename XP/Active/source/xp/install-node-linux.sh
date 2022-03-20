48216cb8f8c903210ed6dff04ca9a654c4673242
Kaeon ACE <kaeon.ace@gmail.com>
2022-03-20 09:35:53 -0400

    Kaeon

---
+++ XP/Active/source/xp/install-node-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+arm = $(uname -m)
+
+if which node > /dev/null
+	then
+		echo "node is installed, skipping..."
+	else
+		if [arm == "armv6l"]
+			then
+				wget https://nodejs.org/dist/v11.15.0/node-v11.15.0-linux-armv6l.tar.xz
+				tar -xJf node-v11.15.0-linux-armv6l.tar.xz
+				sudo cp -r node-v11.15.0-linux-armv6l/* /usr/local/
+				rm -r node-v11.15.0-linux-armv6l
+			else
+				sudo apt-get update
+				sudo apt-get install nodejs
+			fi
+	fi
